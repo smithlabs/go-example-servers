1232d0b1b8b90fcd624da3eead72269ab42119d1
Sean Smith <40581796+smithlabs@users.noreply.github.com>
2020-11-09 19:26:44 -0700

    Adds a newline at the end of Hello, World so my GIF with a curl example looks better

--- 01-hello-world/main.go
+++ 01-hello-world/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
     http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello world!")
+        fmt.Fprintf(w, "Hello, world!\n")
     })
     http.ListenAndServe(":8080", nil)
 }
